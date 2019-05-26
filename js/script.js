0346a8d0904a03f8305b7e3b07401895558e7348
Gleb Danilenko <glebdanilenko@gmail.com>
2019-05-26 05:06:21 +0300

    slohitebalo

--- js/script.js
+++ js/script.js
@@ -2,8 +2,15 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (currentScrollPos < 100) {
-    document.getElementById("navbar").className ='navbar navbar-expand-lg navbar-light fixed-top';
+    document.getElementById("navbar").className = 'navbar navbar-expand-lg navbar-light fixed-top';
   } else {
     document.getElementById("navbar").className = "navbar navbar-expand-lg navbar-light fixed-top scrolled";
   }
 }
+/* var revitem = 0;
+function slide () {
+var revItem = document.querySelectorAll('.review');
+console.log (revItem);
+return revItem;
+}
+document.addEventListener("DOMContentLoaded", slide ); */
