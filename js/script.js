c4370ef4ab2cf964970140d9139c8067d6aa1e70
Gleb Danilenko <glebdanilenko@gmail.com>
2019-05-29 00:33:13 +0300

    324423

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ window.onscroll = function () {
     document.getElementById("navbar").className = "navbar navbar-expand-lg navbar-light fixed-top scrolled";
   }
 }
+
 $(document).ready(function(){
   $('.gallery').slick({
     arrows:false,
@@ -47,46 +48,7 @@ $(document).ready(function(){
     ]
   });
 });
-$(document).ready(function(){
-  $('.slide-sert').slick({
-    arrows:false,
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
-});
+
 
 $(document).ready(function(){
   $('.reviews-box').slick({
@@ -98,9 +60,9 @@ $(document).ready(function(){
     slidesToScroll: 1,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 1284,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
           dots: false
