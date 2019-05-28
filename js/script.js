627cf220ab91542c2b9324f35bf121eb0fd20e2c
Gleb Danilenko <glebdanilenko@gmail.com>
2019-05-28 06:02:16 +0300

    323

--- js/script.js
+++ js/script.js
@@ -7,13 +7,15 @@ window.onscroll = function () {
     document.getElementById("navbar").className = "navbar navbar-expand-lg navbar-light fixed-top scrolled";
   }
 }
-/* $(document).ready(function(){
+$(document).ready(function(){
   $('.gallery').slick({
     dots: true,
     infinite: true,
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
     responsive: [
       {
         breakpoint: 1024,
@@ -44,4 +46,42 @@ window.onscroll = function () {
     ]
   });
 });
- */
+$(document).ready(function(){
+  $('.slide-sert').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
