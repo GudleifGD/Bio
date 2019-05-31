48912ed4b4dc54c141be99d18f8ad290b40adcf9
Gleb Danilenko <glebdanilenko@gmail.com>
2019-05-31 10:09:49 +0300

    432432432432

--- js/script.js
+++ js/script.js
@@ -31,17 +31,17 @@ $(document).ready(function () {
     responsive: [{
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 1,
           infinite: true,
           dots: true
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 769,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 1
         }
       },
       {
@@ -164,4 +164,26 @@ jQuery(document).ready(function ($) {
     });
     return false;
   });
+});
+
+$(document).ready(function() { // Ждём загрузки страницы
+	
+	$(".image").click(function(){
+    	// Событие клика на маленькое изображение
+	  	var img = $(this);	// Получаем изображение, на которое кликнули
+		var src = img.attr('src'); // Достаем из этого изображения путь до картинки
+		$("body").append("<div class='popup'>"+ //Добавляем в тело документа разметку всплывающего окна
+						 "<div class='popup_bg'></div>"+ // Блок, который будет служить фоном затемненным
+						 "<img src='"+src+"' class='popup_img' />"+ // Само увеличенное фото
+             "</div>"); 
+             
+    $(".popup").fadeIn(800); // Медленно выводим изображение
+		$(".popup_bg").click(function(){	// Событие клика на затемненный фон	   
+			$(".popup").fadeOut(800);	// Медленно убираем всплывающее окно
+			setTimeout(function() {	// Выставляем таймер
+			  $(".popup").remove(); // Удаляем разметку всплывающего окна
+			}, 800);
+		});
+	});
+	
 });
